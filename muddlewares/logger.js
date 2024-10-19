c1f27cda94b51f11c8a363fae9dcce8dd9b7cf4c
Sujit Singh <147229100+SujitSingh521@users.noreply.github.com>
2024-10-19 04:02:16 +0000

    superhero-admin

--- muddlewares/logger.js
+++
@@ -1,9 +0,0 @@
-// middlewares/logger.js
-const logger = (req, res, next) => {
-    const timestamp = new Date().toString();
-    console.log(`URL: ${req.url}, Method: ${req.method}, Timestamp: ${timestamp}`);
-    next();
-  };
-  
-  module.exports = logger;
-  
